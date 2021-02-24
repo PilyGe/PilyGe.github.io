dd30ab42a58e19f1121be949b19789a0e59bb545
PilyGe <79184181+PilyGe@users.noreply.github.com>
2021-02-24 11:57:25 +1100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,30 @@
+window.onload= function (){
+  parent.window.scrollTo(0,0);
+}
+
+// form 
 
 const form = document.querySelector("form");
 const errorsList = document.querySelector("#errors");
 
 form.addEventListener("submit", validateForm);
 
+
 function validateForm(event) {
-  // prevent page refresh
+
   event.preventDefault();
   
-  // clear out any previous errors
+ 
   errorsList.innerHTML = "";
   
-  // validate first name
+ 
   let firstName = document.querySelector("#first-name").value;
   if (firstName.length < 2) {
-    // create separate function to add `li` element to `errors` ul - shortens the code
+   
     addError("First name must be at least 2 characters");
   }
   
-  // validate last name
+ 
   let lastName = document.querySelector("#last-name").value;
   if (lastName.length < 2) {
     addError("Last name must be at least 2 characters");
@@ -29,4 +35,5 @@ function addError(error) {
   let li = document.createElement("li");
   li.innerText = error;
   errorsList.appendChild(li);
+}
 }
